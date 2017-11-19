5e740c836cd69251b564f58e7f28976840d95829
MikhailSmv <smirnov_mikhail_69@mail.ru>
2017-11-20 00:32:41 +0300

    Internet

--- app/src/main/java/com/loftscool/moneytracker/MainActivity.java
+++ app/src/main/java/com/loftscool/moneytracker/MainActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +18,10 @@ public class MainActivity extends AppCompatActivity {
         TabLayout tabs = findViewById(R.id.tabs);
         pager.setAdapter(new MainPagerAdapter(getSupportFragmentManager(),getResources()));
         tabs.setupWithViewPager(pager);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
 
     }
 
